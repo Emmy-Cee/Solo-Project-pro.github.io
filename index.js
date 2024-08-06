716ddd2a7cc8c6610294639f209c5b230e271d8a
Emmanuel Atat <164534642+Emmy-Cee@users.noreply.github.com>
2024-08-06 16:25:17 +0100

    Updated files

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+// Set the initial time in seconds
+let time = 0;
+let timer = document.getElementById("time")
+// Create a function to update the timer every second
+function updateTimer() {
+  // Update the time and format it as minutes and seconds
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  // Display the formatted time on the console or on your webpage
+  timer.textContent = (`${minutes}:${seconds}`);
+  // Increase the time by one second
+  time++;
+
+}
+// Call the updateTimer function every second (1000 milliseconds for 1 second)
+setInterval(updateTimer, 1000);
+
 let count = 0
 let output1 = document.getElementById("home-scores")
 function add1() {
@@ -26,4 +43,26 @@ function add2Value() {
 function add3Value() {
     value += 3
     output2.textContent = value
+}
+
+//Pop-up message response-code: not working
+if (timer === "48:0") {
+    time = 0
+    let homeScore = output1;
+    let guestScore = output2;
+    // let messageOutput = document.getElementById("message")
+    // Function to identify the team with the highest score
+        function getHighestScoringTeam() {
+        if (homeScore > guestScore) {
+            // messageOutput.textContent = "Whoo! The Home team has won😃😃" + "Guest team better luck next time👍👍"
+            console.log("Whoo! The Home team has won😃😃" + "Guest team better luck next time👍👍")
+        return messageOutput;
+        } else if (guestScore > homeScore) {
+            // messageOutput.textContent = "Whoo! The Guest team has won😃😃" + "Home team strive harder👍👍"
+            console.log("Whoo! The Guest team has won😃😃" + "Home team strive harder👍👍")
+        return messageOutput;
+        } else {
+        return "Both teams have the same score";
+        }
+        }
 }
